55f817501c64049f6c78cedfaadf4178f09000ee
saulsantana0412 <saulsantan0412@gmail.com>
2026-05-09 17:53:18 -0400

    primer commit

---
+++ src/mis-reservas/page.js
@@ -0,0 +1,8 @@
+export default function MisReservas() {
+    return (
+        <div>
+            <h2 className="text-3xl font-bold mb-4">Mis Reservas</h2>
+            <p>Aquí iran mis reservas.</p>
+        </div>
+    )
+}
